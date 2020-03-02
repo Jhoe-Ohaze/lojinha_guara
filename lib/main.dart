ee31fcbeba74130949974cc9d3214e195bc3ce22
Jhoe Ohaze <ohaze.jhoe@gmail.com>
2020-03-02 15:49:32 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:lojinha_guara/screens/home_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp
+      (
+      title: 'Flutter Demo',
+      theme: ThemeData
+        (
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:lojinha_guara/tabs/home_tab.dart';
+import 'package:lojinha_guara/widgets/custom_drawer.dart';
+
+class HomeScreen extends StatelessWidget
+{
+  final _pageController = PageController();
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return PageView
+    (
+      controller: _pageController,
+      physics: NeverScrollableScrollPhysics(),
+      children: <Widget>
+      [
+        Scaffold
+        (
+          body: ProductTab(),
+          drawer: CustomDrawer(_pageController),
+        ),
+        Scaffold
+        (
+          body: Container(color: Colors.red,),
+          drawer: CustomDrawer(_pageController),
+        ),
+        Scaffold
+        (
+          body: Container(color: Colors.blue,),
+          drawer: CustomDrawer(_pageController),
+        ),
+        Scaffold
+        (
+          body: Container(color: Colors.green,),
+          drawer: CustomDrawer(_pageController),
+        ),
+      ],
+    );
+  }
+}
