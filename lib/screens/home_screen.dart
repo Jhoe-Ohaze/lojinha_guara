cb0241ff1cd19143c5bc7e6f15b91c0be8deafbf
Jhoe Ohaze <ohaze.jhoe@gmail.com>
2020-03-16 16:58:40 -0300

    changes 13/03

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,63 +9,61 @@ class HomeScreen extends StatelessWidget
 {
   final _pageController = PageController();
   final _titleController = TextEditingController();
-  final _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context)
   {
     _titleController.text = "Início";
-    return Stack
-    (
-      key: _scaffoldKey,
-      children: <Widget>
-      [
-        Scaffold
-          (
-          drawer: CustomDrawer(_pageController, _titleController),
-          body: PageView
-            (
-            physics: NeverScrollableScrollPhysics(),
-            controller: _pageController,
-            children: <Widget>
-            [
-              HomeTab(),
-              TicketTab(_pageController, _titleController),
-              ExcursionScreen(),
-              Container(color: Colors.red),
-              Container(color: Colors.yellow),
-              Container(color: Colors.green)
-            ],
-          ),
-        ),
-        SizedBox
+    return Scaffold
+      (
+        drawer: CustomDrawer(_pageController, _titleController),
+        body: Column
         (
-          height: MediaQuery.of(context).size.height*0.20,
-          child: Stack
-          (
-            children: <Widget>
-            [
-              Image.asset('my_assets/menu.png', width: MediaQuery.of(context).size.width, fit: BoxFit.fill,),
-              Row
+          children: <Widget>
+          [
+            SizedBox
+            (
+              height: MediaQuery.of(context).size.height*0.20,
+              child: Stack
               (
-                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>
                 [
-                  Container
+                  Image.asset('my_assets/menu.png', width: MediaQuery.of(context).size.width, fit: BoxFit.fill,),
+                  Row
                   (
-                    margin: EdgeInsets.symmetric(horizontal: 15),
-                    child: GestureDetector
-                      (
-                      child: Icon(Icons.menu, color: Colors.white, size: 30,),
-                      onTap: (){_scaffoldKey.currentState.openDrawer();},
-                    ),
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>
+                    [
+                      Container
+                        (
+                        margin: EdgeInsets.symmetric(horizontal: 15),
+                        child: GestureDetector
+                          (
+                          child: Icon(Icons.menu, color: Colors.white, size: 30,),
+                          onTap: (){Scaffold.of(context).openDrawer();},
+                        ),
+                      )
+                    ],
                   )
                 ],
-              )
-            ],
-          ),
+              ),
+            ),
+            PageView
+            (
+              physics: NeverScrollableScrollPhysics(),
+              controller: _pageController,
+              children: <Widget>
+              [
+                HomeTab(),
+                TicketTab(_pageController, _titleController),
+                ExcursionScreen(),
+                Container(color: Colors.red),
+                Container(color: Colors.yellow),
+                Container(color: Colors.green)
+              ],
+            ),
+          ],
         )
-      ],
     );
   }
 }
